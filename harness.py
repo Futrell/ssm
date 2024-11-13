6b42da1da61e6e6980ba8e3da62ae19813d260ec
luckyentropy <hutengdai@gmail.com>
2024-11-12 19:19:26 -0500

    ok

---
+++ harness.py
@@ -0,0 +1,14 @@
+# process sequence data into one-hot vectors
+
+def load(file_path):
+    wordlist = []
+    return wordlist
+
+
+def word_to_vec():
+
+    phone2ix = {}
+    
+    return vector
+
+
