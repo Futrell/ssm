bbb2149b5e94bf98e4b98e1022caaa097bdc310c
Richard Futrell <canjo@Richards-MacBook-Pro-2.local>
2024-12-03 16:40:26 -0800

    add more models to model eval

--- eval_model.py
+++ eval_model.py
@@ -1,14 +1,32 @@
 import csv
 import argparse
+from typing import *
 
 import ssm
 import process_data
 
-def get_model(model_type: str, vocab_size: int):
+def get_model(
+        model_type: str,
+        vocab_size: int,
+        state_dim: Optional[int] = None,
+) -> ssm.PhonotacticsModel:
+    if state_dim is None:
+        state_dim = vocab_size
+        
     if model_type == 'sl2':
         return ssm.SL2.initialize(vocab_size)
+    elif model_type == 'sp2':
+        return ssm.SP2.initialize(vocab_size)
+    elif model_type == 'soft_tsl2':
+        return ssm.SoftTSL2.initialize(vocab_size)
+    elif model_type == 'ssm':
+        return ssm.SSMPhonotacticsModel.initialize(state_dim, vocab_size)
+    elif model_type == 'pfsa':
+        return ssm.PFSAPhonotacticsModel.initialize(state_dim, vocab_size)
+    elif model_type == 'wfsa':
+        return ssm.WFSAPhonotacticsModel.initialize(state_dim, vocab_size)
     else:
-        crash
+        raise ValueError("Unrecognized model type: %s" % model_type)
 
 parser = argparse.ArgumentParser(description="Train a phonotactics model.")
 parser.add_argument('model_type', type=str, help="Model type, a string")
@@ -18,6 +36,7 @@ parser.add_argument('--col_separator', type=str, default="\t", help="Delimiter f
 parser.add_argument('--test_file', type=str, default=None, help="Test data, two columns where first is a form and second is a judgment")
 parser.add_argument('--batch_size', type=int, default=1, help="Batch size")
 parser.add_argument('--num_epochs', type=int, default=1, help="Number of iterations through training data")
+parser.add_argument('--lr', type=float, default=.001, help="Adam learning rate")
 parser.add_argument('--report_every', type=int, default=1, help="How often to report training results")
 parser.add_argument('--reporting_window_size', type=int, default=100, help="Window size for averaging for reporting diagnostics")
 
@@ -27,6 +46,7 @@ args = parser.parse_args()
     col_separator=args.col_separator,
     char_separator=args.char_separator
 )
+
 vocab_size = max(map(max, data)) + 1
 # TODO: Read in test data, use it to formulate good vs. bad eval to be passed in as as diagnostic fn
 batches = ssm.minibatches(data, args.batch_size, args.num_epochs)
@@ -34,7 +54,8 @@ model = get_model(args.model_type, vocab_size)
 model.train(
     batches,
     report_every=args.report_every,
-    reporting_window_size=args.reporting_window_size
+    reporting_window_size=args.reporting_window_size,
+    lr=args.lr,
 )
 
 
