345158197d3eaf1f50e7708aea5c7ada3faa1904
Connor Mayer <cjmayer@uci.edu>
2024-12-03 19:41:51 -0800

    Added test function, split data into good/bad

--- eval_model.py
+++ eval_model.py
@@ -28,35 +28,69 @@ def get_model(
     else:
         raise ValueError("Unrecognized model type: %s" % model_type)
 
-parser = argparse.ArgumentParser(description="Train a phonotactics model.")
-parser.add_argument('model_type', type=str, help="Model type, a string")
-parser.add_argument('train_file', type=str, help="Training data, only TRUE cases will be used")
-parser.add_argument('--char_separator', type=str, default="", help="Delimiter for characters")
-parser.add_argument('--col_separator', type=str, default="\t", help="Delimiter for forms vs. judgments in data files")
-parser.add_argument('--test_file', type=str, default=None, help="Test data, two columns where first is a form and second is a judgment")
-parser.add_argument('--batch_size', type=int, default=1, help="Batch size")
-parser.add_argument('--num_epochs', type=int, default=1, help="Number of iterations through training data")
-parser.add_argument('--lr', type=float, default=.001, help="Adam learning rate")
-parser.add_argument('--report_every', type=int, default=1, help="How often to report training results")
-parser.add_argument('--reporting_window_size', type=int, default=100, help="Window size for averaging for reporting loss")
+def get_vocab_size(data):
+    # Each symbol is represented by an integer, starting from 0. To get vocab
+    # size, find the largest integer used and add 1.
+    good_vocab_size = max(map(max, data[True])) + 1
+    bad_vocab_size = max(map(max, data[False])) + 1
+    return max(good_vocab_size, bad_vocab_size)
 
-args = parser.parse_args()
-*_, data = process_data.process_data(
-    args.train_file,
-    col_separator=args.col_separator,
-    char_separator=args.char_separator,
-)
+def test_eval(test_data):
+    def compute_good_scores(model):
+        return model.log_likelihood(test_data[True]).item()
+        
+    def compute_bad_scores(model):
+       return model.log_likelihood(test_data[False]).item()
 
-vocab_size = max(map(max, data)) + 1
-# TODO: Read in test data, use it to formulate good vs. bad eval to be passed in as as diagnostic fn
-batches = ssm.minibatches(data, args.batch_size, args.num_epochs)
-model = get_model(args.model_type, vocab_size)
-model.train(
-    batches,
-    report_every=args.report_every,
-    reporting_window_size=args.reporting_window_size,
-    lr=args.lr,
-)
+    return {
+        'good_scores': compute_good_scores, 
+        'bad_scores': compute_bad_scores
+    }
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a phonotactics model.")
+    parser.add_argument('model_type', type=str, help="Model type, a string")
+    parser.add_argument('train_file', type=str, 
+                        help="Training data, only TRUE cases will be used")
+    parser.add_argument('test_file', type=str, default=None, 
+                        help="""Test data, two columns where first is a form 
+                                and second is a judgment""")
+    parser.add_argument('--char_separator', type=str, default="", 
+                        help="Delimiter for characters")
+    parser.add_argument('--col_separator', type=str, default="\t", 
+                        help="Delimiter for forms vs. judgments in data files")
+    parser.add_argument('--batch_size', type=int, default=1, help="Batch size")
+    parser.add_argument('--num_epochs', type=int, default=1, 
+                        help="Number of iterations through training data")
+    parser.add_argument('--lr', type=float, default=.001, 
+                        help="Adam learning rate")
+    parser.add_argument('--report_every', type=int, default=1, 
+                        help="How often to report training results")
+    parser.add_argument('--reporting_window_size', type=int, default=100, 
+                        help="Window size for averaging for reporting loss")
 
+    args = parser.parse_args()
 
+    *_, train_data = process_data.process_data(
+        args.train_file,
+        col_separator=args.col_separator,
+        char_separator=args.char_separator,
+    )
+    vocab_size = get_vocab_size(train_data)
+
+    *_, test_data = process_data.process_data(
+        args.test_file,
+        col_separator=args.col_separator,
+        char_separator=args.char_separator,
+    )
+    test_eval = test_eval(test_data)
+
+    batches = ssm.minibatches(train_data[True], args.batch_size, args.num_epochs)
+    model = get_model(args.model_type, vocab_size)
+    model.train(
+        batches,
+        report_every=args.report_every,
+        reporting_window_size=args.reporting_window_size,
+        lr=args.lr,
+        diagnostic_fns=test_eval
+    )
--- process_data.py
+++ process_data.py
@@ -1,36 +1,43 @@
 import numpy as np
 import csv
 
+from collections import defaultdict
+
 def load(file_path, col_separator, char_separator):
-    wordlist = []
+    wordlist = defaultdict(list)
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter=col_separator)
         for row in reader:
-            if row:  # Ensure the row is not empty
+            # Ensure the row is not empty
+            if row:
+                row_val = False if row[1] == 'FALSE' else True
+
                 # If char_separator is None, split word character by character
                 if not char_separator:
-                    wordlist.append(list(row[0]))
+                    wordlist[row_val].append(list(row[0]))
                 else:
                     # Split the first column (word) using the char_separator
-                    wordlist.append(row[0].split(char_separator))
+                    wordlist[row_val].append(row[0].split(char_separator))
 
     return wordlist
 
 def build_phone2ix(wordlist):
     # Build a mapping from symbols to integer indices
+    wordlist = wordlist[True] + wordlist[False]
     unique_phones = sorted(set(symbol for word in wordlist for symbol in word))
-    phone2ix = {phone: idx + 1 for idx, phone in enumerate(unique_phones)}
+    phone2ix = {phone: idx for idx, phone in enumerate(unique_phones)}
     return phone2ix
 
 def wordlist_to_vec(wordlist, phone2ix):
-    return [list(map(phone2ix.get, word)) for word in wordlist]
+    good = [list(map(phone2ix.get, word)) for word in wordlist[True]]
+    bad = [list(map(phone2ix.get, word)) for word in wordlist[False]]
+    return {True: good, False: bad}
 
 def process_data(file_path, col_separator=",", char_separator=" "):
     wordlist = load(file_path, col_separator, char_separator)
     phone2ix = build_phone2ix(wordlist)
     word_vec = wordlist_to_vec(wordlist, phone2ix)
     return wordlist, phone2ix, word_vec
-# Example usage:
 
 if __name__ == "__main__":
     file_path  = 'space_sep.txt'
